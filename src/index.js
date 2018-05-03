0edeca88c5955c369d5443a1039955fa2d489374
Alchrist <alchristleo96@gmail.com>
2018-05-03 17:17:07 +0700

    add user schema, user api route handler, and user credential validation, and generate user token

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import Promise from 'bluebird';
+
+import auth from './routes/auth';
 
 const app = express();
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost:27017/tokocrypto");
+
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
