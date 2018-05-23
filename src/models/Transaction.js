fe6400f8aa6ef1bfc42fb9f472fd2cc4c3b02c9a
Alchrist <alchristleo96@gmail.com>
2018-05-24 00:28:05 +0700

    added timestamp to transaction schema

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -20,7 +20,7 @@ const schema = new mongoose.Schema(
         },
         userId: { type: mongoose.Schema.Types.ObjectId, required: true }
     },
-    //{ timestamps: true }
+    { timestamps: true }
 );
 
 export default mongoose.model('Transaction', schema)
