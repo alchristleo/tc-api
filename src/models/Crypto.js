1f1a21f13bca99b4e3e6acb22a29901b9daedaa4
alchristleo <alchristleo96@gmail.com>
2018-06-12 16:46:50 +0700

    added timestamp on crypto fetch

--- src/models/Crypto.js
+++ src/models/Crypto.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({
-	id: { type: String, required:true },
-	name: { type: String, required:true },
-	market_cap_usd: {type: Number, required:true},
-	price_btc: { type: Number, required:true },
-	price_usd: { type: Number, required:true },
-	percent_change_24h: { type: Number, required:true },
-    symbol: { type: String, required:true },
-    rank: { type: Number, required:true},
+	id: { type: String, required: true },
+	name: { type: String, required: true },
+	market_cap_usd: { type: Number, required: true },
+	price_btc: { type: Number, required: true },
+	price_usd: { type: Number, required: true },
+	percent_change_24h: { type: Number, required: true },
+	symbol: { type: String, required: true },
+	rank: { type: Number, required: true },
+	last_updated: { type: Date, required: true }
 });
 
 export default mongoose.model('Crypto', schema)
--- src/routes/cryptos.js
+++ src/routes/cryptos.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import rp from 'request-promise';
-import CoinMarketCap  from 'coinmarketcap-api';
+import CoinMarketCap from 'coinmarketcap-api';
 import Crypto from '../models/Crypto';
 import parseErrors from '../utils/parseErrors';
 //import authenticate from '../middlewares/authenticate';
@@ -9,28 +9,38 @@ const router = express.Router();
 //router.use(authenticate);
 
 router.get("/", (req, res) => {
-	// rp('https://api.coinmarketcap.com/v2/ticker/?start=1&limit=100')
+        // rp('https://api.coinmarketcap.com/v2/ticker/?start=1&limit=100')
         // .then(results => res.json({
         //         id: results.data['*']    
         // }))
         // .catch(err => console.log(err));
-        client.getTicker({start: 0, limit: 100})
-        .then(results => res.json({
-                cryptos: results.map(function(item){
-                        return {
-                                id: item.id,
-                                name: item.name,
-                                market_cap_usd: item.market_cap_usd, 
-                                price_btc: item.price_btc,
-                                price_usd: item.price_usd,
-                                percent_change_24h: item.percent_change_24h,
-                                symbol: item.symbol,
-                                rank: item.rank                               
-                        }
-                })
-        }))
-        .catch(console.error)
+        client.getTicker({ start: 0, limit: 100 })
+                .then(results => res.json({
+                        cryptos: results.map(function (item) {
+                                return {
+                                        id: item.id,
+                                        name: item.name,
+                                        market_cap_usd: item.market_cap_usd,
+                                        price_btc: item.price_btc,
+                                        price_usd: item.price_usd,
+                                        percent_change_24h: item.percent_change_24h,
+                                        symbol: item.symbol,
+                                        rank: item.rank,
+                                        last_updated: item.last_updated
+                                }
+                        })
+                }))
+                .catch(console.error)
 });
+
+router.get("/infobox-ticker", (req, res) => {
+        client.getTicker({ start: 0, limit: 100 })
+                .then(results => res.json({
+                        cryptos: results.map(function (item) {
+                                return item
+                        })
+                }));
+})
 
 // router.post("/", (req, res) => {
 // 	Crypto.create({ crypto, userId: req.currentUser._id })
@@ -39,40 +49,40 @@ router.get("/", (req, res) => {
 // });
 
 router.get("/bitcoin-price", (req, res) => {
-        client.getTicker({start: 0, limit: 1})
-        .then(results => res.json({
-                cryptos: results.map(function(item){
-                        return {
-                                id: item.id,
-                                name: item.name,
-                                market_cap_usd: item.market_cap_usd, 
-                                price_btc: item.price_btc,
-                                price_usd: item.price_usd,
-                                percent_change_24h: item.percent_change_24h,
-                                symbol: item.symbol,                            
-                        }
-                })
-        }))
-        .catch(console.error)
+        client.getTicker({ start: 0, limit: 1 })
+                .then(results => res.json({
+                        cryptos: results.map(function (item) {
+                                return {
+                                        id: item.id,
+                                        name: item.name,
+                                        market_cap_usd: item.market_cap_usd,
+                                        price_btc: item.price_btc,
+                                        price_usd: item.price_usd,
+                                        percent_change_24h: item.percent_change_24h,
+                                        symbol: item.symbol,
+                                }
+                        })
+                }))
+                .catch(console.error)
 });
 
 router.get("/current-crypto", (req, res) => {
-        client.getTicker({start: 0, limit: 100})
-        .then(results => res.json({
-                cryptos: results.map(function(item){
-                        return {
-                                id: item.id,
-                                name: item.name,
-                                market_cap_usd: item.market_cap_usd,  
-                                price_btc: item.price_btc,
-                                price_usd: item.price_usd,
-                                percent_change_24h: item.percent_change_24h,
-                                symbol: item.symbol,
-                                rank: item.rank                               
-                        }
-                })
-        }))
-        .catch(console.error)
+        client.getTicker({ start: 0, limit: 100 })
+                .then(results => res.json({
+                        cryptos: results.map(function (item) {
+                                return {
+                                        id: item.id,
+                                        name: item.name,
+                                        market_cap_usd: item.market_cap_usd,
+                                        price_btc: item.price_btc,
+                                        price_usd: item.price_usd,
+                                        percent_change_24h: item.percent_change_24h,
+                                        symbol: item.symbol,
+                                        rank: item.rank
+                                }
+                        })
+                }))
+                .catch(console.error)
 });
 
 export default router;
